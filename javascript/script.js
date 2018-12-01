a501457930f906965456cfe65fd125a0d4867632
Rasan <rashmedo@gmail.com>
2018-12-01 05:13:16 -0500

    first load

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+// select with click function
+
+
+$("ul").on("click", "li" ,function (){
+
+$(this).toggleClass("completed")
+
+});
+
+//click on X to delete
+
+$("ul").on ("click", "span" ,function(event){
+
+    $(this).parent().fadeOut(500,function() {
+
+        $(this).remove ()   
+   
+   
+       })
+   
+    event.stopPropagation()
+
+    
+
+});
+
+$("input[type='text']").keypress (function (event){
+if (event.which ===13) {
+// gets the value of the text entered
+    todoText = $(this).val()
+
+    $(this).val ("");
+// create a new li to add to the task list
+
+$("ul").append ("<li><span><i class= 'fa fa-trash'></i><span> " + todoText+"</li>");
+
+
+}
+
+});
+
+$(".fa-plus").click(function(){
+
+    $("input[type='text']").fadeToggle()
+})
